74e29e3b631eccf328a3dc34147c2e0f2670652b
Brandon Barnette <bcb0065@auburn.edu>
2019-08-27 14:08:29 -0500

    Updated code so it isn't shit.
    Also to practice using Git.

--- src/main/java/JUnitTest.java
+++ src/main/java/JUnitTest.java
@@ -8,13 +8,13 @@ public class JUnitTest {
     public void getMaxPass() {
         hello.Finder finder = new hello.Finder();
         int[] arr = {1, 2, 3, 4, 5, 6};
-        assertEquals(6, hello.Finder.findMax(arr));
+        assertEquals(6, finder.findMax(arr));
     }
 
     @Test
     public void getMinPass() {
         hello.Finder finder = new hello.Finder();
         int[] arr = {1, 2, 3, 4, 5, 6};
-        assertEquals(1, hello.Finder.findMin(arr));
+        assertEquals(1, finder.findMin(arr));
     }
 }
